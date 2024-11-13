58829b01db22d0be76c441c6d764511e69585adc
zurailiridze123 <zurailuridze2@gmail.com>
2024-11-13 17:37:09 +0400

    secend commit

--- day 35/claswork/claswork.py
+++ day 35/claswork/claswork.py
@@ -1,1 +1,4 @@
-
+def is_even(n): 
+    if isinstance(n , float)and not int():
+        return False
+    return n % 2 == 0
---
+++ day 35/claswork/claswork5.py
@@ -0,0 +1,5 @@
+def make_negative( number ):
+    return -abs(number)
+print(make_negative(1))  
+print(make_negative(-5))
+print(make_negative(0)) 

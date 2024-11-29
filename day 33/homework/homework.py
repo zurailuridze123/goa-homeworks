50635fba1d589f695e2e1e8d70055a12fa906530
zurailiridze123 <zurailuridze2@gmail.com>
2024-11-29 15:51:26 +0400

    commit

--- day 33/homework/homework.py
+++ day 33/homework/homework.py
@@ -1,31 +1,30 @@
+# 'Classwork 2: List Functions and Methods
 
 
+# Create a list named numbers that contains the following integers: 10, 20, 30, 40, 50, 60, 70, 80, 90.
+numbers = [10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90]
+# Use the append() function to add the number 100 to the end of the list.
+numbers = [1 , 2 , 3 , 4 , 5]
+numbers.append(100)
+print(numbers)
+# Use the insert() function to add the number 5 at the beginning of the list.
+numbers = [10 , 20 , 30 , 40]
+numbers.insert(0 , 5)
+print(numbers)
 
-# Classwork 2: List Functions and Methods
-
-
-# 1. Create a list named numbers that contains the following integers: 10, 20, 30, 40, 50, 60, 70, 80, 90.
-
-
-# 2. Use the append() function to add the number 100 to the end of the list.
-
-
-
-# 3. Use the insert() function to add the number 5 at the beginning of the list.
-
-
-# 4. Use the remove() function to remove the number 30 from the list.
-
-
-
-# 5. Use the reverse() function to reverse the order of the list.
-
-
-
-
-
-# 6. Use the index() function to find the index of the number 50.
-
-
-
-# 7. Use the count() function to count how many times 20 appears in the list.
+# Use the remove() function to remove the number 30 from the list.
+numbers = [10 , 20 , 30 , 40 , 50]
+numbers.remove(30)
+print(numbers)
+# Use the reverse() function to reverse the order of the list.
+numbers = [1 , 2 , 3 , 4 , 5]
+numbers.reverse()
+print(numbers)
+# Use the index() function to find the index of the number 50.
+numbers = [10 , 20 , 30 , 40 , 50]
+numbers.index(50)
+print(numbers)
+# Use the count() function to count how many times 20 appears in the list.
+numbers = [10 , 20 , 30]
+numbers.count(20)
+print(numbers)
